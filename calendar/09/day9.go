0955c8235e5cc1b29fd3a091d9afdc2a56ab04b2
Diego Sanchez <diegosanchezlamas@gmail.com>
2021-12-10 08:34:46 +0100

    improve day 9

--- calendar/09/day9.go
+++ calendar/09/day9.go
@@ -14,10 +14,7 @@ import (
 func toIntMatrix(input []string) [][]int {
 	result := make([][]int, len(input))
 	for i, line := range input {
-		result[i] = make([]int, len(line))
-		for j, num := range line {
-			result[i][j] = int(num - '0')
-		}
+		result[i] = mapper.ToIntSlice(strings.Split(line, ""))
 	}
 	return result
 }
@@ -48,53 +45,19 @@ func main() {
 
 func sumLowPoints(input [][]int) (int, error) {
 	var result int
+	iDirection := [4]int{-1, 1, 0, 0} // up, down, left, right
+	jDirection := [4]int{0, 0, -1, 1} // up, down, left, right
 	for i, row := range input {
 		for j, num := range row {
-			if i == 0 {
-				if j == 0 {
-					if num < input[i+1][j] && num < input[i][j+1] {
-						result += num + 1
-					}
-				} else if j == len(row)-1 {
-					if num < input[i+1][j] && num < input[i][j-1] {
-						result += num + 1
-					}
-				} else {
-					if num < input[i+1][j] && num < input[i][j-1] && num < input[i][j+1] {
-						result += num + 1
-					}
+			isLowPoint := true
+			for moveIdx, iMove := range iDirection {
+				iPos, jPos := i+iMove, j+jDirection[moveIdx]
+				if 0 <= iPos && iPos < len(input) && 0 <= jPos && jPos < len(row) && input[iPos][jPos] <= num {
+					isLowPoint = false
+					break
 				}
-				continue
 			}
-			if i == len(input)-1 {
-				if j == 0 {
-					if num < input[i-1][j] && num < input[i][j+1] {
-						result += num + 1
-					}
-				} else if j == len(row)-1 {
-					if num < input[i-1][j] && num < input[i][j-1] {
-						result += num + 1
-					}
-				} else {
-					if num < input[i-1][j] && num < input[i][j-1] && num < input[i][j+1] {
-						result += num + 1
-					}
-				}
-				continue
-			}
-			if j == 0 {
-				if num < input[i-1][j] && num < input[i+1][j] && num < input[i][j+1] {
-					result += num + 1
-				}
-				continue
-			}
-			if j == len(row)-1 {
-				if num < input[i-1][j] && num < input[i+1][j] && num < input[i][j-1] {
-					result += num + 1
-				}
-				continue
-			}
-			if num < input[i-1][j] && num < input[i+1][j] && num < input[i][j-1] && num < input[i][j+1] {
+			if isLowPoint {
 				result += num + 1
 			}
 		}
@@ -104,30 +67,16 @@ func sumLowPoints(input [][]int) (int, error) {
 
 func findBasin(i, j int, input [][]int) []string {
 	var basinPositions []string
-	// up
-	if i > 0 {
-		if input[i-1][j] > input[i][j] && input[i-1][j] < 9 {
+
+	iDirection := [4]int{-1, 1, 0, 0} // up, down, left, right
+	jDirection := [4]int{0, 0, -1, 1} // up, down, left, right
+	for moveIdx, iMove := range iDirection {
+		iPos, jPos := i+iMove, j+jDirection[moveIdx]
+		if 0 <= iPos && iPos < len(input) && 0 <= jPos && jPos < len(input[0]) && input[iPos][jPos] > input[i][j] && input[iPos][jPos] < 9 {
 			basinPositions = append(basinPositions, fmt.Sprintf("%d,%d", i-1, j))
 		}
 	}
-	// down
-	if i < len(input)-1 {
-		if input[i+1][j] > input[i][j] && input[i+1][j] < 9 {
-			basinPositions = append(basinPositions, fmt.Sprintf("%d,%d", i+1, j))
-		}
-	}
-	// left
-	if j > 0 {
-		if input[i][j-1] > input[i][j] && input[i][j-1] < 9 {
-			basinPositions = append(basinPositions, fmt.Sprintf("%d,%d", i, j-1))
-		}
-	}
-	// right
-	if j < len(input[0])-1 {
-		if input[i][j+1] > input[i][j] && input[i][j+1] < 9 {
-			basinPositions = append(basinPositions, fmt.Sprintf("%d,%d", i, j+1))
-		}
-	}
+
 	for _, basinPosition := range basinPositions {
 		position := mapper.ToIntSlice(strings.Split(basinPosition, ","))
 		basinPositions = append(basinPositions, findBasin(position[0], position[1], input)...)
@@ -146,93 +95,25 @@ func findBasin(i, j int, input [][]int) []string {
 }
 
 func multiplyBasins(input [][]int) (int, error) {
-	basinSizes := make([]int, 3)
+	var basinSizes []int
+	iDirection := [4]int{-1, 1, 0, 0} // up, down, left, right
+	jDirection := [4]int{0, 0, -1, 1} // up, down, left, right
 	for i, row := range input {
 		for j, num := range row {
-			if i == 0 {
-				if j == 0 {
-					if num < input[i+1][j] && num < input[i][j+1] {
-						basinSize := len(findBasin(i, j, input)) + 1
-						if basinSize > basinSizes[0] {
-							basinSizes[0] = basinSize
-						}
-						sort.Ints(basinSizes)
-					}
-				} else if j == len(row)-1 {
-					if num < input[i+1][j] && num < input[i][j-1] {
-						basinSize := len(findBasin(i, j, input)) + 1
-						if basinSize > basinSizes[0] {
-							basinSizes[0] = basinSize
-						}
-						sort.Ints(basinSizes)
-					}
-				} else {
-					if num < input[i+1][j] && num < input[i][j-1] && num < input[i][j+1] {
-						basinSize := len(findBasin(i, j, input)) + 1
-						if basinSize > basinSizes[0] {
-							basinSizes[0] = basinSize
-						}
-						sort.Ints(basinSizes)
-					}
+			isLowPoint := true
+			for moveIdx, iMove := range iDirection {
+				iPos, jPos := i+iMove, j+jDirection[moveIdx]
+				if 0 <= iPos && iPos < len(input) && 0 <= jPos && jPos < len(row) && input[iPos][jPos] <= num {
+					isLowPoint = false
+					break
 				}
-				continue
 			}
-			if i == len(input)-1 {
-				if j == 0 {
-					if num < input[i-1][j] && num < input[i][j+1] {
-						basinSize := len(findBasin(i, j, input)) + 1
-						if basinSize > basinSizes[0] {
-							basinSizes[0] = basinSize
-						}
-						sort.Ints(basinSizes)
-					}
-				} else if j == len(row)-1 {
-					if num < input[i-1][j] && num < input[i][j-1] {
-						basinSize := len(findBasin(i, j, input)) + 1
-						if basinSize > basinSizes[0] {
-							basinSizes[0] = basinSize
-						}
-						sort.Ints(basinSizes)
-					}
-				} else {
-					if num < input[i-1][j] && num < input[i][j-1] && num < input[i][j+1] {
-						basinSize := len(findBasin(i, j, input)) + 1
-						if basinSize > basinSizes[0] {
-							basinSizes[0] = basinSize
-						}
-						sort.Ints(basinSizes)
-					}
-				}
-				continue
-			}
-			if j == 0 {
-				if num < input[i-1][j] && num < input[i+1][j] && num < input[i][j+1] {
-					basinSize := len(findBasin(i, j, input)) + 1
-					if basinSize > basinSizes[0] {
-						basinSizes[0] = basinSize
-					}
-					sort.Ints(basinSizes)
-				}
-				continue
-			}
-			if j == len(row)-1 {
-				if num < input[i-1][j] && num < input[i+1][j] && num < input[i][j-1] {
-					basinSize := len(findBasin(i, j, input)) + 1
-					if basinSize > basinSizes[0] {
-						basinSizes[0] = basinSize
-					}
-					sort.Ints(basinSizes)
-				}
-				continue
-			}
-			if num < input[i-1][j] && num < input[i+1][j] && num < input[i][j-1] && num < input[i][j+1] {
+			if isLowPoint {
 				basinSize := len(findBasin(i, j, input)) + 1
-				if basinSize > basinSizes[0] {
-					basinSizes[0] = basinSize
-				}
-				sort.Ints(basinSizes)
+				basinSizes = append(basinSizes, basinSize)
 			}
 		}
 	}
-	return basinSizes[0] * basinSizes[1] * basinSizes[2], nil
+	sort.Ints(basinSizes)
+	return basinSizes[len(basinSizes)-1] * basinSizes[len(basinSizes)-2] * basinSizes[len(basinSizes)-3], nil
 }
